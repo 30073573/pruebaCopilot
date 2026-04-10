379f570b8f1109308aedc6173cf6a7ca66c39d63
Evaristo Ramos Gallego <66991976+30073573@users.noreply.github.com>
2026-04-10 09:43:09 +0200

    Create PruebaCopilotApplication.java main class

---
+++ src/main/java/adeo/lmes/pruebaCopilot/PruebaCopilotApplication.java
@@ -0,0 +1,9 @@
+package adeo.lmes.pruebaCopilot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PruebaCopilotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaCopilotApplication.class, args);
+    }
+}
